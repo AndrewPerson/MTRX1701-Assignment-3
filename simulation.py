9b83c4cee20aa2ac68ac03d9acfb7cd6fe62a648
AndrewPerson <professor_smart@hotmail.com>
2026-04-28 08:31:10 +1000

    Added if __name__ == "__main__" guard to simulation

--- simulation.py
+++ simulation.py
@@ -71,52 +71,53 @@ def simulate(
     return np.array(world_states)
 
 
-DT = 0.05
-INITIAL_STATE = np.zeros(3)
-ROBOT_WIDTH = 0.025
-SENSOR_RADIUS_FROM_CENTER = 0.07905694150420949
-SENSOR_ANGLE_FROM_CENTER = 0.32175055439664224
-ELLIPSE_X = 0
-ELLIPSE_Y = 0.075
-ELLIPSE_RADIUS_A = 0.2
-ELLIPSE_RADIUS_B = 0.075
-ELLIPSE_THICKNESS = 0.015
-V_MAX = 0.00625
+if __name__ == "__main__":
+    DT = 0.05
+    INITIAL_STATE = np.zeros(3)
+    ROBOT_WIDTH = 0.025
+    SENSOR_RADIUS_FROM_CENTER = 0.07905694150420949
+    SENSOR_ANGLE_FROM_CENTER = 0.32175055439664224
+    ELLIPSE_X = 0
+    ELLIPSE_Y = 0.075
+    ELLIPSE_RADIUS_A = 0.2
+    ELLIPSE_RADIUS_B = 0.075
+    ELLIPSE_THICKNESS = 0.015
+    V_MAX = 0.00625
 
-Xs = simulate(
-    np.arange(0, 240, DT),
-    DT,
-    INITIAL_STATE,
-    ROBOT_WIDTH,
-    SENSOR_RADIUS_FROM_CENTER,
-    SENSOR_ANGLE_FROM_CENTER,
-    ELLIPSE_X,
-    ELLIPSE_Y,
-    ELLIPSE_RADIUS_A,
-    ELLIPSE_RADIUS_B,
-    ELLIPSE_THICKNESS,
-    V_MAX
-)
+    Xs = simulate(
+        np.arange(0, 240, DT),
+        DT,
+        INITIAL_STATE,
+        ROBOT_WIDTH,
+        SENSOR_RADIUS_FROM_CENTER,
+        SENSOR_ANGLE_FROM_CENTER,
+        ELLIPSE_X,
+        ELLIPSE_Y,
+        ELLIPSE_RADIUS_A,
+        ELLIPSE_RADIUS_B,
+        ELLIPSE_THICKNESS,
+        V_MAX
+    )
 
-import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
+    import matplotlib.pyplot as plt
+    from matplotlib.patches import Ellipse
 
-plt.figure(figsize=(8, 6))
-plt.plot(Xs[:, 1], Xs[:, 2], label="Robot Trajectory (Bang-Bang)")
+    plt.figure(figsize=(8, 6))
+    plt.plot(Xs[:, 1], Xs[:, 2], label="Robot Trajectory (Bang-Bang)")
 
-# plt.plot(Xs[:, 0], Xs[:, 7])
-# plt.plot(Xs[:, 0], Xs[:, 8])
+    # plt.plot(Xs[:, 0], Xs[:, 7])
+    # plt.plot(Xs[:, 0], Xs[:, 8])
 
-plt.title("Line Following Robot Trajectory")
-plt.xlabel("X Position (m)")
-plt.ylabel("Y Position (m)")
-plt.grid(True)
-plt.legend()
-plt.axis("equal")
+    plt.title("Line Following Robot Trajectory")
+    plt.xlabel("X Position (m)")
+    plt.ylabel("Y Position (m)")
+    plt.grid(True)
+    plt.legend()
+    plt.axis("equal")
 
-ellipse = Ellipse(xy=(ELLIPSE_X, ELLIPSE_Y), width=ELLIPSE_RADIUS_A * 2, height=ELLIPSE_RADIUS_B * 2, 
-                        edgecolor='r', fc='None', lw=2)
+    ellipse = Ellipse(xy=(ELLIPSE_X, ELLIPSE_Y), width=ELLIPSE_RADIUS_A * 2, height=ELLIPSE_RADIUS_B * 2, 
+                            edgecolor='r', fc='None', lw=2)
 
-plt.gca().add_patch(ellipse)
+    plt.gca().add_patch(ellipse)
 
-plt.show()
+    plt.show()
