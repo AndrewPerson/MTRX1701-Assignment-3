8e1a15269478ef1e5ab89eacdca6d14114bd648b
AndrewPerson <professor_smart@hotmail.com>
2026-04-30 13:24:21 +1000

    Changed simulation code to produce both bang-bang and proportional control graphs

--- simulation.py
+++ simulation.py
@@ -18,6 +18,7 @@ def simulate(
     ellipse_radius_b: float,
     thickness: float,
     v_max: float,
+    use_proportional: bool = False
 ):
     world_states = []
 
@@ -48,7 +49,7 @@ def simulate(
         )
 
         [right_vel, left_vel] = control_model(
-            right_sensor_dist, left_sensor_dist, v_max, True
+            right_sensor_dist, left_sensor_dist, v_max, use_proportional=use_proportional
         )
 
         control_vec = control_vector(right_vel, left_vel, robot_width)
@@ -73,6 +74,8 @@ def simulate(
 
 if __name__ == "__main__":
     import math
+    import matplotlib.pyplot as plt
+    from matplotlib.patches import Ellipse
 
     DT = 0.05  # s
     INITIAL_STATE = np.zeros(3)
@@ -94,7 +97,43 @@ if __name__ == "__main__":
     WHEEL_ANGULAR_VELOCITY = 7.645  # rad (Taken from measured values in report)
     V_MAX = WHEEL_RADIUS * WHEEL_ANGULAR_VELOCITY
 
-    Xs = simulate(
+    def plot(states, trajectory_title):
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+
+        ax1.plot(states[:, 1], states[:, 2], label="Robot Path", color="blue")
+
+        ellipse = Ellipse(
+            xy=(ELLIPSE_X, ELLIPSE_Y),
+            width=ELLIPSE_RADIUS_A * 2,
+            height=ELLIPSE_RADIUS_B * 2,
+            edgecolor="r",
+            fc="None",
+            lw=2,
+            label="Target Path",
+        )
+
+        ax1.add_patch(ellipse)
+
+        ax1.set_title(trajectory_title)
+        ax1.set_xlabel("X Position (m)")
+        ax1.set_ylabel("Y Position (m)")
+        ax1.axis("equal")
+        ax1.grid(True)
+        ax1.legend()
+
+        ax2.plot(states[:, 0], states[:, 3] % (2 * math.pi), color="red", label="Heading")
+        ax2.set_title("Vehicle Heading vs. Time")
+        ax2.set_xlabel("Time (s)")
+        ax2.set_ylabel("Angle (rad)")
+        ax2.grid(True)
+        ax2.legend()
+
+        fig.tight_layout()
+
+        return fig
+    
+
+    states = simulate(
         np.arange(0, 240, DT),
         DT,
         INITIAL_STATE,
@@ -109,42 +148,24 @@ if __name__ == "__main__":
         V_MAX,
     )
 
-    import matplotlib.pyplot as plt
-    from matplotlib.patches import Ellipse
+    plot(states, "Robot Trajectory (Bang-Bang Control)")
+    plt.show()
 
-    # Xs = simulate(np.arange(0, 240, DT), DT, INITIAL_STATE, d, r, th, x0, y0, a, b, T, v_max)
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
-
-    ax1.plot(Xs[:, 1], Xs[:, 2], label="Robot Path", color="blue")
-    # ax1.plot(Xs[:, 6], Xs[:, 7], label='Right Sensor Path', color='green')
-    # ax1.plot(Xs[:, 8], Xs[:, 9], label='Left Sensor Pat', color='green')
-
-    ellipse = Ellipse(
-        xy=(ELLIPSE_X, ELLIPSE_Y),
-        width=ELLIPSE_RADIUS_A * 2,
-        height=ELLIPSE_RADIUS_B * 2,
-        edgecolor="r",
-        fc="None",
-        lw=2,
-        label="Target Path",
+    states = simulate(
+        np.arange(0, 240, DT),
+        DT,
+        INITIAL_STATE,
+        ROBOT_WIDTH,
+        SENSOR_RADIUS_FROM_CENTER,
+        SENSOR_ANGLE_FROM_CENTER,
+        ELLIPSE_X,
+        ELLIPSE_Y,
+        ELLIPSE_RADIUS_A,
+        ELLIPSE_RADIUS_B,
+        ELLIPSE_THICKNESS,
+        V_MAX,
+        use_proportional=True
     )
 
-    ax1.add_patch(ellipse)
-
-    ax1.set_title("Robot Trajectory (Bang-Bang Control)")
-    ax1.set_xlabel("X Position (m)")
-    ax1.set_ylabel("Y Position (m)")
-    ax1.axis("equal")
-    ax1.grid(True)
-    ax1.legend()
-
-    ax2.plot(Xs[:, 0], Xs[:, 3] % (2 * math.pi), color="red", label="Heading")
-    ax2.set_title("Vehicle Heading vs. Time")
-    ax2.set_xlabel("Time (s)")
-    ax2.set_ylabel("Angle (rad)")
-    ax2.grid(True)
-    ax2.legend()
-
-    plt.tight_layout()
+    plot(states, "Robot Trajectory (Proportional Control)")
     plt.show()
